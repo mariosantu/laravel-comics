1b50ad779e792a42b151bf7c5f86f40063c442ed
Mario Santucci <mario.santucci95@gmail.com>
2021-06-08 15:31:12 +0200

    added header

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,3 +1,58 @@
 <header>
-    ciao sono lheader
+
+    <div class="header-container wrapper">
+
+
+        <div class="logo">
+            <img src="{{ asset('img/dc-logo.png')}}" alt="dc logo">
+        </div>
+    
+        <nav>
+            <ul class="menu">
+                <li>
+                    <a href="#">CHARACTERS</a>
+                </li>
+    
+                <li>
+                    <a href="#">COMICS</a>
+                </li>
+    
+                <li>
+                    <a href="#">MOVIES</a>
+                </li>
+    
+                <li>
+                    <a href="#">TV</a>
+                </li>
+    
+                <li>
+                    <a href="#">GAMES</a>
+                </li>
+    
+                <li>
+                    <a href="#">COLLECTIBLES</a>
+                </li>
+    
+                <li>
+                    <a href="#">VIDEOS</a>
+                </li>
+    
+                <li>
+                    <a href="#">FANS NEWS</a>
+                </li>
+    
+                <li>
+                    <a href="#">SHOP</a>
+                </li>
+            </ul>
+        </nav>
+        
+        <div class="search-bar-container">
+            <input type="search" placeholder="Search">
+        </div>
+    </div>
+
+
+    <div class="jumbotron-container">
+    </div>
 </header>
