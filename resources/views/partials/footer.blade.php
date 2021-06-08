303118526c0e17c98a42a90da7cbfb7011ed2147
Mario Santucci <mario.santucci95@gmail.com>
2021-06-08 19:05:29 +0200

    added footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,1 +1,196 @@
-<h2>ciao sono il footer</h2>
+<footer>
+
+    <section class="footer-top">
+        <div class="dc-bg">
+            <div class="footer-list-container">
+                <h2>DC COMICS</h2>
+        
+                <div class="sx-list">
+                    <ul>
+                        <li>
+                            <a href="#">characters</a>
+                        </li>
+        
+                        <li>
+                            <a href="#">Comics</a>
+                        </li>
+        
+                        <li>
+                            <a href="#">Movies</a>
+                        </li>
+        
+                        <li>
+                            <a href="#">characters</a>
+                        </li>
+        
+                        <li>
+                            <a href="#">TV</a>
+                        </li>
+        
+                        <li>
+                            <a href="#">Games</a>
+                        </li>
+        
+                        <li>
+                            <a href="#">Videos</a>
+                        </li>
+        
+                        <li>
+                            <a href="#">News</a>
+                        </li>
+        
+                        <li class="sx-list-bottom">
+                            <h2>DC SHOP</h2>
+        
+                            <ul>
+                                <li>
+                                    <a href="#">shop DC</a>
+                                </li>
+        
+                                <li>
+                                    <a href="#">shop DC collectibles</a>
+                                </li>
+                            </ul>
+        
+        
+                        </li>
+                    </ul>
+                </div>
+        
+                <div class="center-list">
+        
+                    <h2>DC</h2>
+        
+                    <ul>
+                        <li>
+                            <a href="#">Terme Of Use</a>
+                        </li>
+        
+                        <li>
+                            <a href="#">Privacy policy (New)</a>
+                        </li>
+        
+                        <li>
+                            <a href="#">Ad Choice</a>
+                        </li>
+        
+                        <li>
+                            <a href="#">Advertising</a>
+                        </li>
+        
+                        <li>
+                            <a href="#">Jobs</a>
+                        </li>
+        
+                        <li>
+                            <a href="#">Subscriptions</a>
+                        </li>
+        
+                        <li>
+                            <a href="#">Talent Workshops</a>
+                        </li>
+        
+                        <li>
+                            <a href="#">CPSC Certificates</a>
+                        </li>
+        
+                        <li>
+                            <a href="#">Ratings</a>
+                        </li>
+        
+                        <li>
+                            <a href="#">Shop Help</a>
+                        </li>
+        
+                        <li>
+                            <a href="#">Contact us</a>
+                        </li>
+                    </ul>
+                </div>
+        
+                <div class="dx-list">
+        
+                    <h2>SITES</h2>
+        
+                    <ul>
+                        <li>
+                            <a href="#">DC</a>
+                        </li>
+        
+                        <li>
+                            <a href="#">MAD Magazine</a>
+                        </li>
+        
+                        <li>
+                            <a href="#">DC kids</a>
+                        </li>
+        
+                        <li>
+                            <a href="#">DC universe</a>
+                        </li>
+        
+                        <li>
+                            <a href="#">DC power visa</a>
+                        </li>
+                    </ul>
+                </div>
+        
+            </div>
+        
+            <div class="copyright-container wrapper">
+                <p>All Site Content TM and © 2020 DC Entertainment, unless otherwise <a href="#">noted here</a>.All rights reserved. <br>
+                   <a href="#">Cookies Settings</a>
+                </p>
+            </div>
+        </div>
+    </section>
+
+    <section class="footer-bot">
+
+        <div class="footer-bot-container wrapper">
+            <div class="sign-up">
+                <a href="#">SIGN UP NOW</a> 
+            </div>
+    
+            <div class="socials">
+                <div class="socials-title">
+                    <p>FOLLOW US</p>
+                </div>
+                
+    
+                <ul>
+                    <li>
+                        <a href="#">
+                            <img src="{{ asset('img/footer-facebook.png')}}" alt="">
+                        </a>
+                    </li>
+    
+                    <li>
+                        <a href="#">
+                            <img src="{{ asset('img/footer-twitter.png')}}" alt="">
+                        </a>
+                    </li>
+    
+                    <li>
+                        <a href="#">
+                            <img src="{{ asset('img/footer-youtube.png')}}" alt="">
+                        </a>
+                    </li>
+    
+                    <li>
+                        <a href="#">
+                            <img src="{{ asset('img/footer-pinterest.png')}}" alt="">
+                        </a>
+                    </li>
+    
+                    <li>
+                        <a href="#">
+                            <img src="{{ asset('img/footer-periscope.png')}}" alt="">
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        
+    </section>
+</footer>
